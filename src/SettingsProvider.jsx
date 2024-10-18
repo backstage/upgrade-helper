c908ca7b234d425b1d6e1b67d8c27a101a8d5f61
MT Lewis <mtlewis@users.noreply.github.com>
2024-10-18 16:33:33 +0100

    Switch to query param for yarn plugin setting (#16)
    * updateURL improvements
    
    - Make parameters independent from one another by
      using the URL constructor rather than string
      concatenation to build up the URL.
    - Switch from `replaceState` to `pushState` so
      users can navigate between different
      configurations using the browser forward and
      back buttons
    - Trigger a `popstate` event after updating the
      URL. This allows hooks like `useSearchParams` to
      pick up the change
    
    * Move yarnPlugin setting to URL
    
    This allows us to send people to the right version
    of the diff depending on whether they have the
    yarn plugin installed from places like the
    versions:bump command.
    
    * Make settings checkboxes controlled
    
    Guarantees that checkboxes are in sync with the
    underlying settings

--- src/SettingsProvider.jsx
+++ src/SettingsProvider.jsx
@@ -1,23 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext } from 'react'
 import useLocalStorage from 'react-use/lib/useLocalStorage'
+import useSearchParam from 'react-use/lib/useSearchParam'
 
 import { SHOW_LATEST_RCS, USE_YARN_PLUGIN } from './utils'
+import { updateURL } from './utils/update-url'
 
 const INITIAL_STATE = {
-  [`${SHOW_LATEST_RCS}`]: false,
-  [`${USE_YARN_PLUGIN}`]: false
+  [`${SHOW_LATEST_RCS}`]: false
 }
 
 export const SettingsContext = React.createContext(INITIAL_STATE)
 
 export const SettingsProvider = React.memo(function({ children }) {
-  const [settings, setSettings] = useLocalStorage(
+  const useYarnPlugin = useSearchParam('yarnPlugin')
+
+  const [settings, setLocalStorageSettings] = useLocalStorage(
     'backstage:upgrade-helper:settings',
     INITIAL_STATE
   )
 
+  const setUseYarnPlugin = useYarnPlugin => {
+    updateURL({ yarnPlugin: useYarnPlugin })
+  }
+
+  const setSettings = useCallback(settings => {
+    const {
+      [USE_YARN_PLUGIN]: newUseYarnPlugin,
+      ...localStorageSettings
+    } = settings
+
+    if (newUseYarnPlugin !== useYarnPlugin) {
+      setUseYarnPlugin(newUseYarnPlugin)
+    }
+
+    setLocalStorageSettings(localStorageSettings)
+  })
+
   return (
-    <SettingsContext.Provider value={{ settings, setSettings }}>
+    <SettingsContext.Provider
+      value={{
+        settings: {
+          ...settings,
+          [USE_YARN_PLUGIN]: !!JSON.parse(useYarnPlugin)
+        },
+        setSettings
+      }}
+    >
       {children}
     </SettingsContext.Provider>
   )
