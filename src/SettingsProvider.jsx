493edc2f5b4a683c70ed798e43b8efb205c3e766
Gökhan Alkacir <alkacirg@gmail.com>
2025-01-24 19:47:45 +0100

    feat: use localstorage instead of url params for yarn checkbox

--- src/SettingsProvider.jsx
+++ src/SettingsProvider.jsx
@@ -1,47 +1,30 @@
 import React, { useCallback, useContext } from 'react'
 import useLocalStorage from 'react-use/lib/useLocalStorage'
-import useSearchParam from 'react-use/lib/useSearchParam'
 
 import { SHOW_LATEST_RCS, USE_YARN_PLUGIN } from './utils'
-import { updateURL } from './utils/update-url'
 
 const INITIAL_STATE = {
-  [`${SHOW_LATEST_RCS}`]: false
+  [`${SHOW_LATEST_RCS}`]: false,
+  [`${USE_YARN_PLUGIN}`]: false
 }
 
 export const SettingsContext = React.createContext(INITIAL_STATE)
 
 export const SettingsProvider = React.memo(function({ children }) {
-  const useYarnPlugin = useSearchParam('yarnPlugin')
-
   const [settings, setLocalStorageSettings] = useLocalStorage(
     'backstage:upgrade-helper:settings',
     INITIAL_STATE
   )
 
-  const setUseYarnPlugin = useYarnPlugin => {
-    updateURL({ yarnPlugin: useYarnPlugin })
-  }
-
   const setSettings = useCallback(settings => {
-    const {
-      [USE_YARN_PLUGIN]: newUseYarnPlugin,
-      ...localStorageSettings
-    } = settings
-
-    if (newUseYarnPlugin !== useYarnPlugin) {
-      setUseYarnPlugin(newUseYarnPlugin)
-    }
-
-    setLocalStorageSettings(localStorageSettings)
+    setLocalStorageSettings(settings)
   })
 
   return (
     <SettingsContext.Provider
       value={{
         settings: {
-          ...settings,
-          [USE_YARN_PLUGIN]: !!JSON.parse(useYarnPlugin)
+          ...settings
         },
         setSettings
       }}
